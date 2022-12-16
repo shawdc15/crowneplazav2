0c16703a73e2fa6df32a0ae28c0b69fa78ea049d
Marvi Lean Negrete <acemonkey2@gmail.com>
2022-12-17 06:38:18 +0800

    fixed

--- pages/customer/forgot_password/index.js
+++ pages/customer/forgot_password/index.js
@@ -26,7 +26,7 @@ const forgotPassword = () => {
           email: email,
           subject: 'reset_password',
           fullname: `${result.data[0].firstName} ${result.data[0].lastName}`,
-          link: `https://crowne-plaza.herokuapp.com/customer/forgot_password/${result.data[0]._id}`,
+          link: `https://crowneplazav2.vercel.app/customer/forgot_password/${result.data[0]._id}`,
         }
         const request = await sendPasswordLink(newData)
         // console.log(request)
